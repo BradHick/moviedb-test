052cafd052d3794f82733d4be1e1765567f26caa
BradHick <bradleyhrm@gmail.com>
2019-04-12 17:55:20 -0300

    feat: create cast model

---
+++ src/model/cast.js
@@ -0,0 +1,49 @@
+import Immutable from 'seamless-immutable';
+import { API_KEY, URL_DETAIL, URL_CAST } from '../const';
+
+const initialState = new Immutable({
+  loading: false,
+  items:{},
+  error:{}
+});
+
+const cast = {
+  state: initialState,
+  reducers: {
+    fetchCastFulfiled: (state, payload) => {
+      return state.merge({
+        loading: false,
+        items: state.items.concat(payload.results),
+        error: {}
+      });
+    },
+    fetchCastPending: (state) => {
+      return state.merge({
+        loading: true
+      });
+    },
+    resetCast: (state) => {
+      return state.merge({
+        ...initialState
+      });
+    },
+    fetchCastRejected: (state, payload) => {
+      return state.merge({
+        loading: false,
+        error: payload
+      });
+    },
+  },
+  effects: (dispatch) => ({
+    fetchCast: (id) =>{
+      const url = `${URL_DETAIL}${id}${URL_CAST}${API_KEY}`;
+      dispatch.cast.fetchCastPending();
+      return fetch(url)
+        .then(response => response.json())
+        .then(data => dispatch.cast.fetchCastFulfiled(data))
+        .catch(error => dispatch.cast.fetchCastRejected(error));
+    }
+  })
+};
+
+export default cast;

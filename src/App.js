00ad1a49413f095ff4645de192f109f80c7edf64
BradHick <bradleyhrm@gmail.com>
2019-04-16 12:51:20 -0300

    refactor(App): use F7 configs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Framework7 from 'framework7/framework7.esm.bundle';
-import Framework7React, { App } from 'framework7-react';
+import Framework7React, { App, View } from 'framework7-react';
 
 import 'framework7/css/framework7.bundle.css';
 import 'framework7-icons';
 import './App.css';
 
-import Routes from './Routes';
+import Home from './views/home';
+import Movie from './views/movie';
 
 const params = {
   name: 'Movie DB',
@@ -14,7 +15,16 @@ const params = {
   touch: {
     fastClicks: true,
     tapHold: true,
-  }
+  },
+  routes: [{
+    name: 'home',
+    path: '/',
+    component: Home,
+  }, {
+    name: 'movie',
+    path: '/movie/:id',
+    component: Movie,
+  }]
 };
 
 Framework7.use(Framework7React);
@@ -23,7 +33,11 @@ class Application extends Component {
   render() {
     return (
       <App className="App" params={params}>
-        <Routes />
+        <View
+          id='main-view'
+          main
+          url='/'
+        />
       </App>
     );
   }
